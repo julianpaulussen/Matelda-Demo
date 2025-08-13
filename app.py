055e3f9bbb57d403513fe753b1972e69280181d5
Julian <paulussen@campus.tu-berlin.de>
2025-08-13 14:34:55 +0200

    changed intro text

--- app.py
+++ app.py
@@ -8,7 +8,11 @@ apply_base_styles()
 
 st.title("Matelda")
 st.write("Welcome to Matelda!")
-st.markdown("""Read the full paper [here](https://www.openproceedings.org/2025/conf/edbt/paper-98.pdf) or click the button below to continue with configurations.""")
+st.markdown("""
+The underlying paper is available on [OpenProceedings](https://www.openproceedings.org/2025/conf/edbt/paper-98.pdf).
+The Repository of the Demo is available on [GitHub](https://github.com/lejuliennn/Matelda-Demo).
+Click the button below to start the demo.
+""")
 
 # Sidebar Navigation
 render_sidebar()
